d691f8c701b892b59b69ed28d0a35621b255be73
EdwinMarroquin <60857232+devem-software@users.noreply.github.com>
2024-12-19 21:08:28 -0500

    update

---
+++ assets/SettingsAppView-Dhr_ALdo.js
@@ -0,0 +1,1 @@
+import{_ as t,c as o,a as r,o as n}from"./index-CmJ9Rvnk.js";const s={};function c(a,e){return n(),o("div",null,e[0]||(e[0]=[r("h1",{color:"red"},"Settings App",-1)]))}const i=t(s,[["render",c]]);export{i as default};
